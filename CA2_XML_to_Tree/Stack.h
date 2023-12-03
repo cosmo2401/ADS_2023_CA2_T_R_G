7a72941cd2df93dd7521c2fcef5900f21c51195e
cosmo2401 <100962088+cosmo2401@users.noreply.github.com>
2023-12-03 15:47:58 +0000

    :pencil: Using the stack that we got provided in courses

--- CA2_XML_to_Tree/Stack.h
+++ CA2_XML_to_Tree/Stack.h
@@ -1,45 +1,98 @@
-#pragma once
-
-#include "DList.h"
-
 using namespace std;
 
+#pragma once
 template <class T>
 class Stack
 {
+	T* data;
+	int capacity;
+	int topNum;
 public:
-	DList<T>* stackList;
-
-	Stack();
-	void Push(T item);
-	void Pop();
-	T Top();
-	int Count();
+	Stack(int i = 50);
+	bool push(T item);
+	T top();
+	void pop();
+	int count();
+	bool isEmpty();
+	void clear();
+	void print();
+	~Stack();
 };
 
-template<class T>
-Stack<T>::Stack()
+template <class T>
+void Stack<T>::print()
 {
-	stackList = new DList<T>;
+	for (int i = 0; i < topNum; i++)
+	{
+		if (i != 0)
+		{
+			cout << ", ";
+		}
+		cout << data[i];
+	}
+	cout << endl;
 }
-
-template<class T>
-void Stack<T>::Push(T item)
+template <class T>
+Stack<T>::Stack(int i)
 {
-	stackList->prepend(item);
+	data = new T[i];
+	capacity = i;
+	topNum = 0;
 }
 
 template <class T>
-void Stack<T>::Pop() {
-	stackList->removeHead();
+Stack<T>::~Stack()
+{
+	topNum = 0;
+	capacity = 0;
+	delete[] data;
 }
 
 template <class T>
-T Stack<T>::Top() {
-	return stackList->head->data;
+void Stack<T>::clear()
+{
+	topNum = 0;
 }
 
 template <class T>
-int Stack<T>::Count() {
-	stackList->count;
+bool Stack<T>::push(T item)
+{
+	if (topNum != capacity)
+	{
+		data[topNum] = item;
+		topNum++;
+		return true;
+	}
+	return false;
+}
+
+template <class  T>
+T  Stack<T>::top()
+{
+	if (topNum > 0)
+	{
+		return data[topNum - 1];
+	}
+	return 0;
+}
+
+template <class  T>
+void  Stack<T>::pop()
+{
+	if (topNum > 0)
+	{
+		topNum--;
+	}
+}
+
+template <class T>
+int Stack<T>::count()
+{
+	return topNum;
+}
+
+template <class T>
+bool Stack<T>::isEmpty()
+{
+	return topNum == 0;
 }
