dba4b104adce1a8982d5d3f6880e6c6bdca6237a
cosmo2401 <100962088+cosmo2401@users.noreply.github.com>
2023-12-03 15:47:31 +0000

    :sparkles: Completed the validation function
    :pencil:  Edited the testing string to now have a billion blank spaces
    :pencil: Edited the validateXML function to return a bool instead of void
    :sparkles: Populated the validateXML function

--- CA2_XML_to_Tree/CA2_XML_to_Tree.cpp
+++ CA2_XML_to_Tree/CA2_XML_to_Tree.cpp
@@ -9,114 +9,154 @@
 
 using namespace std;
 
+bool valdiateXML(string data) {
+	Stack<string>* xmlStack = new Stack<string>;
 
-int main()
-{
+	int i = 0;
 
-    string xml = R"(<dir>
-        < name > ADS_Single_LinkedList_Exercises< / name>
-        <dir>
-        <name>.git< / name>
-        <file>
-        <name>config< / name>
-        < length>353 b< / length>
-        <type>config< / type>
-        < / file>
-        <file>
-        <name>description< / name>
-        < length>73 b< / length>
-        <type>description< / type>
-        < / file>
-        <file>
-        <name>HEAD< / name>
-        < length>23 b< / length>
-        <type>HEAD< / type>
-        < / file>
-        < / dir>
-        <dir>
-        <name>.vs< / name>
-        <dir>
-        <name>ADS_Single_LinkedList_Exercises< / name>
-        <dir>
-        <name>v17< / name>
-        < / dir>
-        < / dir>
-        < / dir>
-        <dir>
-        <name>Debug< / name>
-        <file>
-        <name>SinglyLinkedList.pdb< / name>
-        < length>978944 b< / length>
-        <type>pdb< / type>
-        < / file>
-        <file>
-        <name>TestSinglyLinkedList.exp< / name>
-        < length>41528 b< / length>
-        <type>exp< / type>
-        < / file>
-        <file>
-        <name>TestSinglyLinkedList.pdb< / name>
-        < length>1945600 b< / length>
-        <type>pdb< / type>
-        < / file>
-        < / dir>
-        < / dir>)";
+	while (i < data.length()) {
 
-    Stack<string>* newStack = new Stack<string>;
+		if (data[i] == '<') {
+			i++;
 
-    newStack->Push("testing");
+			if (data[i] == '/') { //Check the closing tag name and compare if it is valid
+				i++;
 
-    cout << newStack->Top();
+				string tagName;
 
-    if (newStack->Top() == "test") {
-        cout << " its the same";
-    }
+				for (; data[i] != '>'; i++) tagName += data[i];
 
-    //std::cout << xml;
+				if (xmlStack->isEmpty() || xmlStack->top() != tagName) {
+					cout << "-" << xmlStack->top()<< "-" << endl;
+					cout << "-" << tagName << "-" <<endl;
+					cout << xmlStack->count()  << endl;
+					cout << "tags do not match" << endl;
+					return false;
+				} 
+				
+				xmlStack->pop();
+			}
+			else { //´Check the opening tag name and push it on the stack
+				string tagName;
 
+				for(; data[i] != '>';i++) tagName += data[i];
 
+				xmlStack->push(tagName);
+			}
+		}
 
-}
+		i++;
+	}
 
-void valdiateXML(string data) {
-
+	// If we reach this point everything worked so far now we check if the stack is empty
+	// If stack is empty that means that there were not enough closing tags
+	if (xmlStack->isEmpty()) return true;
+	return false;
 }
 
 void createTree(string data) {
-
 }
 
 void nrOfItems() {
-
 }
 /*
 template <class T>
 void printDFS(TreeIterator<T> iter) //Pre order traversel
 {
-    DListIterator<Tree<T>*> iter = iter;
+	DListIterator<Tree<T>*> iter = iter;
 
-    cout << iter.item() << " ";
-    while (iter.childValid()) {
-        TreeIterator<T> iter2(iter);
-        iter2.down();
-        printDFS(iter2);
-        iter.childForth();
-    }
+	cout << iter.item() << " ";
+	while (iter.childValid()) {
+		TreeIterator<T> iter2(iter);
+		iter2.down();
+		printDFS(iter2);
+		iter.childForth();
+	}
 }
 
 template <class T>
 void calcMemoryForFolder(TreeIterator<T> iter)
 {
-    queue<Tree<T>*> q;
-    q.push(&tree);
-    while (!q.empty()) {
-        cout << q.front()->getData() << " ";
-        DListIterator<Tree<T>*> iter = q.front()->children->getIterator();
-        while (iter.isValid()) {
-            q.push(iter.item());
-            iter.advance();
-        }
-        q.pop();
-    }
+	queue<Tree<T>*> q;
+	q.push(&tree);
+	while (!q.empty()) {
+		cout << q.front()->getData() << " ";
+		DListIterator<Tree<T>*> iter = q.front()->children->getIterator();
+		while (iter.isValid()) {
+			q.push(iter.item());
+			iter.advance();
+		}
+		q.pop();
+	}
 }
 */
+
+int main()
+{
+	string xml = R"(<dir>
+        <name> ADS_Single_LinkedList_Exercises</name>
+        <dir>
+        <name>.git</name>
+        <file>
+        <name>config</name>
+        <length>353 b</length>
+        <type>config</type>
+        </file>
+        <file>
+        <name>description</name>
+        <length>73 b</length>
+        <type>description</type>
+        </file>
+        <file>
+        <name>HEAD</name>
+        <length>23 b</length>
+        <type>HEAD</type>
+        </file>
+        </dir>
+        <dir>
+        <name>.vs</name>
+        <dir>
+        <name>ADS_Single_LinkedList_Exercises</name>
+        <dir>
+        <name>v17</name>
+        </dir>
+        </dir>
+        </dir>
+        <dir>
+        <name>Debug</name>
+        <file>
+        <name>SinglyLinkedList.pdb</name>
+        <length>978944 b</length>
+        <type>pdb</type>
+        </file>
+        <file>
+        <name>TestSinglyLinkedList.exp</name>
+        <length>41528 b</length>
+        <type>exp</type>
+        </file>
+        <file>
+        <name>TestSinglyLinkedList.pdb</name>
+        <length>1945600 b</length>
+        <type>pdb</type>
+        </file>
+        </dir>
+        </dir>)";
+
+	Stack<string>* newStack = new Stack<string>;
+
+	newStack->push("testing");
+
+	cout << newStack->top() << endl;
+	cout << "Length of the xml string " << xml.length() << endl;
+
+	if (newStack->top() == "test") {
+		cout << " its the same";
+	}
+
+	// Testing purposes
+	if (valdiateXML(xml)) {
+		cout << "This is a valid xml" << endl;
+	}
+
+	//std::cout << xml;
+}
